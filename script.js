b4edb647c6717291b1d27ce7bd103d0e7c84ee65
User200925 <dalimovhojiakbar@gmail.com>
2024-11-18 19:08:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Akbar",
+    level: "js",
+    pasword: "000000",
+    tel: 8888888888,
+}
+
+console.log(student.age)
+
+let student1 = {}
+
+student1.name = prompt('Введите имя:');
+student1.level = prompt('Введите space id:');
+student1.pasword = prompt('Введите пароль:')
